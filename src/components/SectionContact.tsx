871c8f827346ce2582d722e743b035bf9fdf60b4
Mac32 <alberth.bompart22@gmail.com>
2023-03-05 21:27:33 -0500

    Agregado de funcion responsive a la seccion de contacto

--- src/components/SectionContact.tsx
+++ src/components/SectionContact.tsx
@@ -1,28 +1,28 @@
-import BackgroundContact from './BackgroundContact'
 import H2 from './H2'
 import HorizontalLine from './HorizontalLine'
 import Label from './Label'
 import TextInput from './TextInput'
-import emailjs, { send } from '@emailjs/browser'
+import emailjs from '@emailjs/browser'
 import React, { useState } from 'react'
-
-export default function SectionContact () {
+export default function SectionContact (): JSX.Element {
   const [templateForm, setTemplateForm] = useState({})
   const [sended, setSended] = useState(false)
 
   if (sended) return (<h3>Mensaje enviado con exito! </h3>)
 
-  const sendEmail = (e) => {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault()
     emailjs.send('service_fgwvyva', 'template_695fg4e', templateForm, 'WD-Lh4KXqoNe1lmJk').then((result) => {
       alert('Correo enviado con exito')
-      e.target.reset()
+      if (e.currentTarget.form != null) {
+        e.currentTarget.form.reset()
+      }
       setSended(true)
     }, (error) => {
       console.log(error.text)
     })
   }
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement >): void => {
     e.preventDefault()
     const { name, value } = e.target
     setTemplateForm({
@@ -37,7 +37,7 @@ export default function SectionContact () {
       <HorizontalLine width='w-3/4' />
 
       <div className='mt-16'>
-        <form className='grid gap-6 w-2/5 mx-auto' onSubmit={sendEmail}>
+        <form className='grid gap-6 w-11/12 md:w-9/12 lg:w-2/5 mx-auto' onSubmit={(e: React.FormEvent<HTMLFormElement>) => sendEmail}>
           <Label htmlFor='name'>Name:</Label>
           <TextInput required onChange={handleChange} name='name' placeholder='Name' />
           <Label htmlFor='email'>Email:</Label>
@@ -48,7 +48,7 @@ export default function SectionContact () {
         </form>
       </div>
 
-      <div className='mx-auto bg-blue-400/20 text-center my-10 w-2/5 rounded-md py-6'>
+      <div className='mx-auto bg-blue-400/20 text-center my-10 w-11/12 md:w-9/12 lg:w-2/5 rounded-md py-6'>
         <p className='text-2xl mb-2'>My email:</p>
         <address className='text-lg underline underline-offset-4'>alberth.bompart22@gmail.com</address>
       </div>
